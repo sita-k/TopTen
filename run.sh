1b9b77424f2a5adc9962a27af4c0b5d7cced2e12
sita-k <35182075+sita-k@users.noreply.github.com>
2018-10-26 23:48:40 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-java Top10H1OccupationsStates input/H1B_FY_2015.csv output/top_10_occupations.txt output/top_10_states.txt
+java /bin/com/insight/Top10H1OccupationsStates input/H1B_FY_2015.csv output/top_10_occupations.txt output/top_10_states.txt
