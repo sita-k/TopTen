003f0ba6837b3728f686cef5dc939e0b034ec86f
sita-k <35182075+sita-k@users.noreply.github.com>
2018-10-27 08:11:26 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pwd
 javac ./src/TopTen.java
 #java -classpath bin TopTen input/H1B_FY_2015.csv output/top_10_occupations.txt output/top_10_states.txt
 #java TopTen input/H1B_FY_2015.csv output/top_10_occupations.txt output/top_10_states.txt
