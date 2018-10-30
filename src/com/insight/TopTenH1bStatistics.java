3d1c89f861e53103d0f0c11ff1af0d399bfebe69
sita-k <35182075+sita-k@users.noreply.github.com>
2018-10-30 08:45:35 -0400

    Add files via upload

--- src/com/insight/TopTenH1bStatistics.java
+++ src/com/insight/TopTenH1bStatistics.java
@@ -12,10 +12,15 @@ import java.util.Map;
 
 public class TopTenH1bStatistics {
 	static long total = 0;
+	/*
+	 * Main method
+	 * 1. reads input args, first arg for input file and next two for output file names
+	 * 2. calls getTopTen() which reads the files into a map and calls outputToFile()
+	 */
 	public static void main(String[] args) {
-		String inFile  = "input/H1B_FY_2015.csv";
-		String occupationsOutFile = "output/top_10_occupations.txt";
-		String statesOutfile = "output/top_10_states.txt";
+		String inFile  = args[0]; //"input/H1B_FY_2015.csv";
+		String occupationsOutFile = args[1]; //"output/top_10_occupations.txt";
+		String statesOutfile = args[2]; //"output/top_10_states.txt";
 		
 		// This delimiter ensures that it does not split the string when the ";" is between quotes
 		String delimiter = ";(?=[^\"]*(?:(?:\"[^\"]*){2})*$)";
@@ -31,6 +36,13 @@ public class TopTenH1bStatistics {
 		top10.getTopTen(inFile, statesOutfile, delimiter, statesHeader, statesDrivingCol);			
 	}
 	
+	
+	/*
+	 * 1. Read the file
+	 * 2. Load the data into a map and filter for "CERTIFIED"
+	 * 3. Sort the data and store sorted data in LinkedHashMap 
+	 * 
+	 */
 	public void getTopTen(String inFile, String outFile, String delimiter, String header, int drivingColumn){
 		try{		
 			Stream<String> lines = Files.lines(Paths.get(inFile));
@@ -62,6 +74,9 @@ public class TopTenH1bStatistics {
 		}	
 	}
 	
+	/*
+	 * 1.Output the header followed by data to the given filename
+	 */
 	public void outputToFile(LinkedHashMap<String, Long> results, String outFileName, String header){
 		try{	
 			total = 0;
